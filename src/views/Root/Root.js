6e3733f6df3300450f22bdb1a24c98b437f0d4ca
Dawid Gola <dawidgola@MacBook-Pro-Dawid.local>
2020-04-14 14:01:24 +0200

    realtive path change eslint and env add and styled components props part 1

---
+++ src/views/Root/Root.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from 'components/Button/Button';
+
+const Root = () => (
+  <p>
+    <Button>close/save</Button>
+    <Button secondary>remove</Button>
+  </p>
+);
+
+export default Root;
